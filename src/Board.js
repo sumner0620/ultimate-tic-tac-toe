51c8198bba915d708027b78cfb741dc13a2bc7a9
dpsumner1289 <dpsumner1289@gmail.com>
2020-01-11 09:56:48 -0500

    refactoring board and boardgrid components

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Square } from './Square';
-import { calculateBoardWinner } from './calculateBoardWinner';
+import React from "react";
+import { Square } from "./Square";
+import { calculateBoardWinner } from "./calculateBoardWinner";
 
 export class Board extends React.Component {
   constructor(props) {
@@ -8,52 +8,68 @@ export class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
       handler: this.props.xHandler.bind(this),
+      active: true
     };
   }
+
+  // Game reset
+  reset = () => {
+    this.setState({
+      squares: Array(9).fill(null)
+    });
+  };
+
+  // Calculate board winner
+
   handleClick(i) {
     const squares = this.state.squares.slice();
-    if (calculateBoardWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.props.xIsNext ? 'X' : 'O';
+    squares[i] = this.props.xIsNext ? "X" : "O";
     this.setState({
-      squares: squares,
+      squares: squares
     });
+    if (calculateBoardWinner(squares)) {
+      alert("winner");
+    }
     const winner = calculateBoardWinner(this.state.squares);
     let status;
     if (winner) {
       status = winner + " wins this board";
+    } else {
+      status = "no winner";
     }
-    else {
-      status = 'no winner';
-    }
-    console.log(status);
     this.props.xHandler();
-    console.log(this.state.squares);
   }
   renderSquare(i) {
-    return (<Square value={this.state.squares[i]} onClick={() => {
-      this.handleClick(i);
-    }} className={"square-" + i} />);
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => {
+          this.handleClick(i);
+        }}
+        className={"square-" + i}
+      />
+    );
   }
   render() {
-    return (<div className="board">
-      <div className="board-row">
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
-        {this.renderSquare(3)}
+    return (
+      <div className="board">
+        <div className="board-row">
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+        </div>
       </div>
-      <div className="board-row">
-        {this.renderSquare(4)}
-        {this.renderSquare(5)}
-        {this.renderSquare(6)}
-      </div>
-      <div className="board-row">
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
-        {this.renderSquare(9)}
-      </div>
-    </div>);
+    );
   }
 }
 // send to BoardGrid
--- src/BoardGrid.js
+++ src/BoardGrid.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import { Board } from './Board';
+import React from "react";
+import { Board } from "./Board";
 
 export class BoardGrid extends React.Component {
   constructor(props) {
     super(props);
     this.handler = this.handler.bind(this);
+    this.resetGame = React.createRef();
     this.state = {
       xIsNext: true
     };
+  }
+  handleReset() {
+    this.resetGame.current.reset();
   }
   handler() {
     this.setState({
@@ -15,25 +19,73 @@ export class BoardGrid extends React.Component {
     });
   }
   render() {
-    const status = 'Next player ' + (this.state.xIsNext ? 'X' : 'O');
-    return (<div>
-      <div className="status">{status}</div>
-      <div className="boards">
-        <Board id={'board-1'} xHandler={this.handler} xIsNext={this.state.xIsNext} />
-        <Board id={'board-2'} xHandler={this.handler} xIsNext={this.state.xIsNext} />
-        <Board id={'board-3'} xHandler={this.handler} xIsNext={this.state.xIsNext} />
+    const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="boards">
+          <Board
+            ref={this.resetGame}
+            id={"board-1"}
+            xHandler={this.handler}
+            xIsNext={this.state.xIsNext}
+          />
+          <Board
+            ref={this.resetGame}
+            id={"board-2"}
+            xHandler={this.handler}
+            xIsNext={this.state.xIsNext}
+          />
+          <Board
+            ref={this.resetGame}
+            id={"board-3"}
+            xHandler={this.handler}
+            xIsNext={this.state.xIsNext}
+          />
+        </div>
+        <div className="boards">
+          <Board
+            ref={this.resetGame}
+            id={"board-4"}
+            xHandler={this.handler}
+            xIsNext={this.state.xIsNext}
+          />
+          <Board
+            ref={this.resetGame}
+            id={"board-5"}
+            xHandler={this.handler}
+            xIsNext={this.state.xIsNext}
+          />
+          <Board
+            ref={this.resetGame}
+            id={"board-6"}
+            xHandler={this.handler}
+            xIsNext={this.state.xIsNext}
+          />
+        </div>
+        <div className="boards">
+          <Board
+            ref={this.resetGame}
+            id={"board-7"}
+            xHandler={this.handler}
+            xIsNext={this.state.xIsNext}
+          />
+          <Board
+            ref={this.resetGame}
+            id={"board-8"}
+            xHandler={this.handler}
+            xIsNext={this.state.xIsNext}
+          />
+          <Board
+            ref={this.resetGame}
+            id={"board-9"}
+            xHandler={this.handler}
+            xIsNext={this.state.xIsNext}
+          />
+        </div>
+        <button onClick={this.handleReset}>Reset Game</button>
       </div>
-      <div className="boards">
-        <Board id={'board-4'} xHandler={this.handler} xIsNext={this.state.xIsNext} />
-        <Board id={'board-5'} xHandler={this.handler} xIsNext={this.state.xIsNext} />
-        <Board id={'board-6'} xHandler={this.handler} xIsNext={this.state.xIsNext} />
-      </div>
-      <div className="boards">
-        <Board id={'board-7'} xHandler={this.handler} xIsNext={this.state.xIsNext} />
-        <Board id={'board-8'} xHandler={this.handler} xIsNext={this.state.xIsNext} />
-        <Board id={'board-9'} xHandler={this.handler} xIsNext={this.state.xIsNext} />
-      </div>
-    </div>);
+    );
   }
 }
 // send to Game (index.js)
